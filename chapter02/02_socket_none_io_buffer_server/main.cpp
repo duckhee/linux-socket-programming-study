96b2f5a0d3ef6094f52133f80bed808722918602
doukhee <fain9301@yahoo.com>
2024-09-09 16:23:50 +0900

    feat: linux socket setting tcp nodelay

---
+++ chapter02/02_socket_none_io_buffer_server/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    int serverSocket, clientSocket;
+    /** server socket create */
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (serverSocket == -1) {
+        cout << "Failed Create socket..." << endl;
+        return -1;
+    }
+
+    sockaddr_in serverInfo = {0,};
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverInfo.sin_port = htons(25000);
+
+    if (bind(serverSocket, (sockaddr *) &serverInfo, sizeof(serverInfo)) == -1) {
+        cout << "Failed Socket Binding... " << endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == -1) {
+        cout << "Failed Socket Listening..." << endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    cout << "Server Started ..." << endl;
+    sockaddr_in clientInfo = {0,};
+    socklen_t clientInfoSize = sizeof(clientInfo);
+    char pszBuffer[128] = {0,};
+    int nReceiveSize = 0;
+
+    while ((clientSocket = accept(serverSocket, (sockaddr *) &clientInfo, &clientInfoSize)) != -1) {
+        cout << "Client Connected !" << endl;
+        while ((nReceiveSize = recv(clientSocket, pszBuffer, 128, 0)) > 0) {
+            send(clientSocket, pszBuffer, 128, 0);
+//write(clientSocket, pszBuffer, 128);
+            puts(pszBuffer);
+            memset(pszBuffer, '\0', sizeof(pszBuffer));
+        }
+
+        shutdown(clientSocket, SHUT_RDWR);
+        close(clientSocket);
+        puts("Client Closed..");
+    }
+
+    close(serverSocket);
+    return 0;
+}
---
+++ chapter02/03_socket_none_io_buffer_client/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <unistd.h>
+#include <memory.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <errno.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    int clientSocket;
+    sockaddr_in serverInfo = {0,};
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (clientSocket == -1) {
+        cout << "Failed Create socket ..." << endl;
+        return -1;
+    }
+
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverInfo.sin_port = htons(25000);
+
+    if (connect(clientSocket, (sockaddr *) &serverInfo, sizeof(serverInfo)) == -1) {
+        cout << "Failed connect server ..." << endl;
+        close(clientSocket);
+        return -1;
+    }
+    int nOpt = 1;
+    /** client socket setting TCP No delay -> not using IO Buffer */
+    setsockopt(
+            clientSocket,
+            IPPROTO_TCP,
+            TCP_NODELAY,
+            (char *) &nOpt,
+            sizeof(nOpt)
+    );
+
+    char pszBuffer[128] = {0,};
+
+    while (true) {
+        fgets(pszBuffer, 128, stdin);
+        if (strcmp(pszBuffer, "EXIT\n") == 0) {
+            break;
+        }
+        int pszLength = strlen(pszBuffer);
+        cout << "Buffer Length : " << pszLength << endl;
+        for (int i = 0; i < pszLength; ++i) {
+            fwrite(pszBuffer + i, sizeof(char), 1, stdout);
+            printf("\r\n");
+//            send(clientSocket, pszBuffer + i, 1, 0);
+            write(clientSocket, (pszBuffer + i), 1);
+        }
+
+        memset(pszBuffer, '\0', sizeof(pszBuffer));
+//        recv(clientSocket, pszBuffer, 128, 0);
+        read(clientSocket, pszBuffer, 128);
+        printf("From Server : %s\r\n", pszBuffer);
+    }
+    shutdown(clientSocket, SHUT_RDWR);
+    close(clientSocket);
+    return 0;
+}
