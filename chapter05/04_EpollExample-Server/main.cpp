a2f8bec397fcacb408b8b34b4f5142a5f05b95e2
DoukheeWon <fain9301@yahoo.com>
2024-10-12 17:20:47 +0900

    feat: need to check epoll

---
+++ chapter05/04_EpollExample-Server/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
+void ErrorHandle(const char *msg);
+
+int main(int argc, char **argv) {
+    return 0;
+}
+
+
+void ErrorHandle(const char *msg) {
+    fputs(msg, stderr);
+    fputc('\r\n', stderr);
+    exit(1);
+}
