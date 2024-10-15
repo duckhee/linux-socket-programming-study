5dc5df419f818c71dfa0c9e8864f4e73e0c68617
doukhee <fain9301@yahoo.com>
2024-10-15 19:22:15 +0900

    feat: set need to test and ftp and http client

---
+++ chapter07/03_FtpClientExample/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+int main(int argc, char **argv) {
+    return 0;
+}
