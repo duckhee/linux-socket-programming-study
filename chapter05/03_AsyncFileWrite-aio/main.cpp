de83d0b56d8ab1a1b40e69f3231aa9045282722c
DoukheeWon <fain9301@yahoo.com>
2024-11-25 14:58:15 +0900

    feat: need to aio test

--- chapter05/03_AsyncFileWrite-aio/main.cpp
+++ chapter05/03_AsyncFileWrite-aio/main.cpp
@@ -19,7 +19,9 @@ void AsyncComplete(sigval_t sig);
 
 void AsyncSignalDo(int);
 
-struct aiocb *g_aioList[1] = {0};
+struct aiocb *g_aioList[1];
+
+pthread_attr_t g_thread_attr;
 
 int main(int argc, char **argv) {
     /** Thread 생성 정보를 담기 위한 구조체 */
@@ -29,7 +31,12 @@ int main(int argc, char **argv) {
     /** set signal detected */
     signal(SIGUSR1, AsyncSignalDo);
     pthread_create(&thread, NULL, ThreadFunction, &fp);
-    pthread_join(thread, 0);
+    pthread_detach(thread);
+//    pthread_join(thread, nullptr);
+    sleep(600);
+
+    aio_suspend(g_aioList, 1, nullptr);
+
     return 0;
 }
 
@@ -47,32 +54,30 @@ void *ThreadFunction(void *pParam) {
     my_aio.aio_offset = 1024 * 1024 * 2; // 2MB Offset
     my_aio.aio_buf = pszBuffer;
     my_aio.aio_nbytes = sizeof(char) * 16;
-    my_aio.aio_sigevent.sigev_notify = SIGEV_THREAD;
-//    my_aio.aio_sigevent.sigev_signo = SIGUSR1;
-//    my_aio.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+//    my_aio.aio_sigevent.sigev_notify = SIGEV_NONE;
+//    my_aio.aio_sigevent.sigev_notify = SIGEV_THREAD;
+    my_aio.aio_sigevent.sigev_signo = SIGUSR1;
+    my_aio.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
 //    my_aio.aio_sigevent.sigev_signo = SIGUSR2;
-
+//    my_aio.aio_sigevent.sigev_value = ;
 //    my_aio.aio_lio_opcode = LIO_WRITE;
     my_aio.aio_sigevent.sigev_notify_function = AsyncComplete;
-//    my_aio.aio_sigevent.sigev_notify_attributes = NULL;
     my_aio.aio_sigevent.sigev_value.sival_ptr = &my_aio;
-
+    g_aioList[0] = (struct aiocb *) malloc(sizeof(struct aiocb));
     g_aioList[0] = {&my_aio};
-    int test = lio_listio(LIO_WAIT, g_aioList, 1, &my_aio.aio_sigevent);
+//    int test = lio_listio(LIO_WAIT, g_aioList, 1, &my_aio.aio_sigevent);
     int ret = aio_write(&my_aio);
 //    aio_cancel(fp, &my_aio);
-//    printf("aio_write : %d\r\n", ret);
-    int result = aio_suspend(g_aioList, 1, nullptr);
+    printf("aio_write : %d\r\n", ret);
+//    int result = aio_suspend(g_aioList, 1, nullptr);
 
-    printf("Done : %d\r\n", result);
+//    printf("Done : %d\r\n", result);
 
-    return 0;
+    return (void *) 0;
 }
 
 void AsyncComplete(union sigval sigval) {
-
     printf("AsyncComplete");
-
     struct aiocb *req;
     req = (struct aiocb *) &sigval.sival_ptr; //Pay attention here.
 //    printf("Async File Write Done : %d\r\n", req->aio_fildes);
@@ -80,19 +85,19 @@ void AsyncComplete(union sigval sigval) {
     if (aio_error(req) == 0) {
 
         /* Request completed successfully, get the return status */
-        int ret = aio_return(req);
-        printf("ret [%d]\n", ret);
-        printf("buff[%s]\n", req->aio_buf);
-        delete[] req->aio_buf;
+
+//        printf("buff[%s]\n", req->aio_buf);
+//        delete[] req->aio_buf;
         close(req->aio_fildes);
     }
 }
 
 void AsyncSignalDo(int sig) {
+
     printf("Test SIGINT Call!\r\n");
-    printf("AsyncSignalDo : %d\r\n", sig);
+    printf("AsyncSignalDo : %s\r\n", (sig == SIGUSR1) ? "SIGUSR1" : "no");
     union sigval *signalValue = (union sigval *) &sig;
-    printf("AsyncSignalDo sigval : %0X\r\n", (char *) signalValue->sival_ptr);
+//    printf("AsyncSignalDo sigval : %s\r\n", (char *) signalValue->sival_ptr);
     struct aiocb *req;
 //    req = (struct aiocb *) signalValue->sival_ptr; //Pay attention here.
     req = g_aioList[0];
@@ -101,12 +106,14 @@ void AsyncSignalDo(int sig) {
     if (aio_error(req) == 0) {
 
         /* Request completed successfully, get the return status */
-        int ret = aio_return(req);
-        printf("ret [%d]\n", ret);
-        printf("buff[%s]\n", (char *) req->aio_buf);
-        delete[] req->aio_buf;
-        close(req->aio_fildes);
+        ssize_t ret = aio_return(req);
+        printf("ret [%ld]\n", ret);
+
+//        printf("fp : %d\r\n", req->aio_fildes ? -1 : req->aio_fildes);
+//        printf("buff[%s]\n", (req->aio_buf == nullptr) ? "not have buffer" : (char *) req->aio_buf);
+//        delete[] req->aio_buf;
+//        close(req->aio_fildes);
     } else {
-        printf("Error code : %d\r\n", aio_error(req));
+        printf("AsyncSignalDo Error code : %d\r\n", aio_error(req));
     }
 }
